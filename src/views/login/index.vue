d873c3d1e20a816583b50fdddfc4738e3b8bcb61
toby20001 <30930565@qq.com>
2020-02-26 11:45:16 +0800

    构建项目、配置路由、elementUI

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">Login</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#login {
+  background-color: #344a5f;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+}
+</style>
